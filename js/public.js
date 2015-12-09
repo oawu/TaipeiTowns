4ff14626478ee95dd26bc09f341c794dc52a9281
OA Wu <comdan66@gmail.com>
2015-12-09 17:36:36 +0800

    Copy form OA-googleMapsMenu

--- js/public.js
+++ js/public.js
@@ -5,4 +5,175 @@
 
 $(function () {
 
+  var $map = $('#map');
+  var $mapMenu = $('#map_menu');
+  var $markerMenu = $('#marker_menu');
+  var $polylineMenu = $('#polyline_menu');
+  var $export = $('#export');
+  var $select = $('#select');
+  var _map = null;
+  var _markers = [];
+ 
+  function formatDate (d) {
+    return  (d.getFullYear () + '_' + (d.getMonth () + 1) + '_' + d.getDate ()) + '_' + (d.getHours () + '_' + d.getMinutes () + '_' + d.getSeconds ());
+  }
+
+  var getPixelPosition = function () {
+    var scale = Math.pow (2, this.map.getZoom ());
+    var nw = new google.maps.LatLng (
+        this.map.getBounds ().getNorthEast ().lat (),
+        this.map.getBounds ().getSouthWest ().lng ()
+    );
+    var worldCoordinateNW = this.map.getProjection ().fromLatLngToPoint (nw);
+    var worldCoordinate = this.map.getProjection ().fromLatLngToPoint (this.getPosition ());
+    
+    return new google.maps.Point (
+        (worldCoordinate.x - worldCoordinateNW.x) * scale,
+        (worldCoordinate.y - worldCoordinateNW.y) * scale
+    );
+  };
+  function formatFloat (num, pos) {
+    var size = Math.pow (10, pos);
+    return Math.round (num * size) / size;
+  }
+  function fromLatLngToPoint (latLng, map) {
+    var scale = Math.pow (2, map.getZoom ());
+    var topRight = map.getProjection ().fromLatLngToPoint (map.getBounds ().getNorthEast ());
+    var bottomLeft = map.getProjection ().fromLatLngToPoint (map.getBounds ().getSouthWest ());
+    var worldPoint = map.getProjection ().fromLatLngToPoint (latLng);
+    return new google.maps.Point ((worldPoint.x - bottomLeft.x) * scale, (worldPoint.y - topRight.y) * scale);
+  }
+  function setPolyline () {
+    for (var i = 0; i < _markers.length; i++) {
+      if (!_markers[i].polyline) {
+        var polyline = new google.maps.Polyline ({
+          map: _map,
+          strokeColor: 'rgba(68, 77, 145, .6)',
+          strokeWeight: 4,
+          drawPath: function () {
+            var prevPosition = this.prevMarker.getPosition ();
+            var nextPosition = this.nextMarker.getPosition ();
+            this.setPath ([prevPosition, nextPosition]);
+            if (!this.prevMarker.map) this.prevMarker.setMap (_map);
+            if (!this.nextMarker.map) this.nextMarker.setMap (_map);
+            if (!this.map) this.setMap (_map);
+          }
+        });
+
+        google.maps.event.addListener (polyline, 'rightclick', function (e) {
+          var point = fromLatLngToPoint (e.latLng, _map);
+          $polylineMenu.css ({ top: point.y, left: point.x })
+                       .data ('lat', e.latLng.lat ())
+                       .data ('lng', e.latLng.lng ())
+                       .addClass ('show').polyline = polyline;
+                              
+        });
+        _markers[i].polyline = polyline;
+      }
+      
+      _markers[i].polyline.prevMarker = _markers[i - 1] ? _markers[i - 1] : _markers[i];
+      _markers[i].polyline.nextMarker = _markers[i];
+      _markers[i].polyline.drawPath ();
+    }
+
+    if (_markers.length)
+      $export.show ();
+    else
+      $export.hide ();
+  }
+  function circlePath (r) {
+    return 'M 0 0 m -' + r + ', 0 '+
+           'a ' + r + ',' + r + ' 0 1,0 ' + (r * 2) + ',0 ' +
+           'a ' + r + ',' + r + ' 0 1,0 -' + (r * 2) + ',0';
+  }
+  function initMarker (position, index) {
+    var marker = new google.maps.Marker ({
+        map: _map,
+        draggable: true,
+        position: position,
+        icon: {
+            path: circlePath (5),
+            strokeColor: 'rgba(50, 60, 140, .4)',
+            strokeWeight: 1,
+            fillColor: 'rgba(68, 77, 145, .95)',
+            fillOpacity: 0.5
+          },
+        getPixelPosition: getPixelPosition
+      });
+
+    google.maps.event.addListener (marker, 'drag', setPolyline);
+
+    google.maps.event.addListener (marker, 'rightclick', function (e) {
+      var pixel = marker.getPixelPosition ();
+      $markerMenu.css ({ top: pixel.y, left: pixel.x }).addClass ('show').marker = marker;
+    });
+    _markers.splice (index ? index : _markers.length, 0, marker);
+    
+    setPolyline ();
+  }
+
+  function initialize () {
+    _map = new google.maps.Map ($map.get (0), {
+        zoom: 11,
+        zoomControl: true,
+        scrollwheel: true,
+        scaleControl: true,
+        mapTypeControl: false,
+        navigationControl: true,
+        streetViewControl: false,
+        disableDoubleClickZoom: true,
+        center: new google.maps.LatLng (25.054, 121.54),
+      });
+
+    google.maps.event.addListener (_map, 'rightclick', function (e) {
+      $mapMenu.css ({ top: e.pixel.y, left: e.pixel.x })
+              .data ('lat', e.latLng.lat ())
+              .data ('lng', e.latLng.lng ()).addClass ('show');
+    });
+    google.maps.event.addListener (_map, 'mousemove', function () {
+      $mapMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+      $markerMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+      $polylineMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+    });
+
+    google.maps.event.addListener (_map, 'click', function (e) {
+      initMarker (e.latLng, 0);
+    });
+
+    $mapMenu.find ('.add_marker').click (function () {
+      initMarker (new google.maps.LatLng ($mapMenu.data ('lat'), $mapMenu.data ('lng')), 0);
+      $mapMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+    });
+
+    $mapMenu.find ('.add_info').click (function () {
+      initInfo (new google.maps.LatLng ($mapMenu.data ('lat'), $mapMenu.data ('lng')));
+      $mapMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+    });
+
+    $markerMenu.find ('.del').click (function () {
+      _markers.splice (_markers.indexOf ($markerMenu.marker), 1);
+      $markerMenu.marker.setMap (null);
+      if ($markerMenu.marker.polyline) $markerMenu.marker.polyline.setMap (null);
+      setPolyline ();
+      $markerMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+    });
+
+    $polylineMenu.find ('.add').click (function () {
+      if ($polylineMenu.polyline)
+        initMarker (new google.maps.LatLng ($polylineMenu.data ('lat'), $polylineMenu.data ('lng')), _markers.indexOf ($polylineMenu.polyline.nextMarker));
+      $polylineMenu.css ({ top: -100, left: -100 }).removeClass ('show');
+    });
+
+
+
+    $export.hide ().click (function () {
+      var a = 'var  = [' + _markers.map (function (marker) {
+        return 'new google.maps.LatLng (' + marker.position.lat () + ', ' + marker.position.lng () + ')'
+      }) + '];';
+      saveAs(new Blob([a], {type: "text/plain;charset=utf-8"}), formatDate (new Date()) + ".txt");
+    });
+
+  }
+
+  google.maps.event.addDomListener (window, 'load', initialize);
 });
